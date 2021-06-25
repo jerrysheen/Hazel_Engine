417db33e52351b37c9060697ced9dab2990f07b6
jerrysheen <jerrysheen@gmail.com>
2021-06-25 22:07:15 +0800

    feat: entry point

---
+++ Hazel/src/Hazel/Log.cpp
@@ -0,0 +1,12 @@
+#include "Log.h"
+//
+//namespace Hazel {
+//
+//	std::shared_ptr<spdlog::logger> s_CoreLogger;
+//	std::shared_ptr<spdlog::logger> s_ClientLogger;
+//
+//	void Log::Init()
+//	{
+//
+//	}
+//}
