92b2b3a70b4008701e7cdccfc886826d0a101396
jerrysheen <jerrysheen@gmail.com>
2021-06-27 19:08:07 +0800

    Event System

---
+++ Hazel/src/Hazel/Events/KeyEvent.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "Event.h"
+
+#include <sstream>
+
+namespace Hazel {
+
+	// 这是一个abstract的event， 因为又protected，所以你没办法创造一个他的类
+	class HAZEL_API KeyEvent : public Event
+	{
+	public:
+		inline int GetKeyCode() const { return m_KeyCode; }
+
+		// = return GetCategoryFlags ...
+		EVENT_CLASS_CATEGORY(EventCategoryKeyboard | EventCategoryInput)
+	protected:
+		KeyEvent(int keycode)
+			: m_KeyCode(keycode) {}
+
+		int m_KeyCode;
+	};
+
+	class HAZEL_API KeyPressedEvent : public KeyEvent
+	{
+	public:
+		// usually the system deal with key pressed event like first get a input,
+		// then pause for a while to deal with the repeat event,
+		// keep pressing a looks like  a aaaaaaaaaaaaaaaaaaaaa. key press event + key repeat event
+		KeyPressedEvent(int keycode, int repeatCount)
+			: KeyEvent(keycode), m_RepeatCount(repeatCount) {}
+
+		inline int GetRepeatCount() const { return m_RepeatCount; }
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "KeyPressedEvent: " << m_KeyCode << " (" << m_RepeatCount << " repeats)";
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyPressed)
+	private:
+		int m_RepeatCount;
+	};
+
+	class HAZEL_API KeyReleasedEvent : public KeyEvent
+	{
+	public:
+		KeyReleasedEvent(int keycode)
+			: KeyEvent(keycode) {}
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "KeyReleasedEvent: " << m_KeyCode;
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyReleased)
+	};
+}
