3425f0267f6971be85542a27a3cb34bc763e2529
shentao <shentao@elex-tech.com>
2022-03-09 21:40:16 +0800

    SceneView grid

--- Sandbox/assets/shaders/Ground.glsl
+++ Sandbox/assets/shaders/Ground.glsl
@@ -35,18 +35,37 @@ uniform vec4 u_Color;
 void main()
 {
 	vec2 temp;
+	vec2 temp1;
+
 	// log10(height);
-	float height = (1 / 3.3219) * log(v_camPos.y);
+	float height = (1 / 3.3219) * log(abs(v_camPos.y));
 	float raction = height - fract(height);
-	temp = fract(v_worldPos.xz / (pow(10, raction)));
-	
-	if(temp.x < 0.01f || temp.y < 0.01f)
+	temp = fract(v_worldPos.xz / (pow(10, raction + 1)));
+	temp1 = fract(v_worldPos.xz / (pow(10, raction)));
+	if(raction < -0.0000001f) discard;
+	// base color : { 104.0/256.0, 97.0 / 256.0, 92.0 / 256.0, 1 }
+	// big grid;
+	vec4 color00;
+	vec4 color01;
+	if(temp.x < 0.0025f || temp.y < 0.0025f) 
 	{
-		color = vec4(116.0/255.0, 113.0/255.0, 116.0/255.0, 1.0);
+		color00 = vec4(116.0/255.0, 113.0/255.0, 116.0/255.0, 1.0);
 	}
 	else
 	{
-		discard;
+		color00 = vec4(104.0/255.0, 97.0 / 255.0, 92.0 / 255.0, 1.0);
 	}
-	//color = vec4(v_camPos.y);
+
+	// small grid;
+	if(temp1.y < 0.015f || temp1.x < 0.015f)
+	{
+		color01 = vec4(116.0/255.0, 113.0/255.0, 116.0/255.0, 1.0);
+	}
+	else
+	{
+		color01 = vec4(104.0/255.0, 97.0 / 255.0, 92.0 / 255.0, 1.0);
+	}
+	//color = color00;
+	//color = color01;
+	color = mix(color01, color00, fract(height));
 }
