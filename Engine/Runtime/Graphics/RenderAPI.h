ea53616729c1343f76d2b30d49eb56f806b6ef44
jerry sheen <jerryshin961115@gmail.com>
2025-06-30 21:53:59 +0800

    [engine] Modify：重构删除冗余代码

--- Engine/Runtime/Graphics/RenderAPI.h
+++ Engine/Runtime/Graphics/RenderAPI.h
@@ -14,15 +14,6 @@ namespace Hazel {
 			None = 0, OpenGL = 1, DirectX12 = 2
 		};
 	public:
-		virtual void Init() = 0;
-		virtual void SetViewPort(uint32_t x, uint32_t y, uint32_t width, uint32_t height) = 0;
-
-		virtual void SetClearColor(const glm::vec4& color) = 0;
-		virtual void Clear() = 0;
-
-		virtual void DrawIndexed(const Ref<VertexArray>& vertexArray) = 0;
-		virtual void DrawLines(const Ref<VertexArray>& vertexArray) = 0;
-		
 		inline static API GetAPI() { return s_API; };
 
 	private:
